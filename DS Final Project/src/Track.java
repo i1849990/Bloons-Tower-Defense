ee445d467e4d0529340bba68925d8856860d3ecc
jayha <jayha@10.9.132.255>
2023-05-19 09:44:29 -0700

    track points added

--- DS Final Project/src/Track.java
+++ DS Final Project/src/Track.java
@@ -5,12 +5,9 @@ public class Track {
 	public int[] xPoints;
 	public int[] yPoints;
 	
-	private BufferedImage image;
-	
-	public Track(int[] xPoints, int[] yPoints, BufferedImage image) {
-		this.xPoints = xPoints;
-		this.yPoints = yPoints;
-		this.image = image;
+	public Track() {
+		this.xPoints = new int[] {-50, 120, 120, 270, 270, 70, 70, 530, 530, 380, 380, 530, 530, 330, 330};
+		this.yPoints = new int[] {320, 320, 470, 470, 170, 170, 60, 60, 230, 230, 370, 370, 530, 530, 650};
 	}
 	
 }
